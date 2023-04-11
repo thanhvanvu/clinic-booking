031f3c20beda8dd013762465cd524c11a190b379
Thanh Vu <65506586+vvt4994@users.noreply.github.com>
2023-04-11 08:31:22 -0500

    Designed a page to show Clinic information

--- front-end/src/containers/Patient/Clinic/ClinicDetail.js
+++ front-end/src/containers/Patient/Clinic/ClinicDetail.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 import './ClinicDetail.scss'
 import DoctorScheduleClinicInfo from '../../../components/DoctorScheduleClinicInfo'
 import { handleGetDoctorInSpecialist } from '../../../services/specialistService'
+import HeaderHomePage from '../../HomePage/HeaderHomePage'
+import img from '../../../assets/clinic/2019_Homepage_Featured.jpg'
+import HomeFooter from '../../HomePage/HomeFooter'
 class ClinicDetail extends Component {
   constructor(props) {
     super(props)
@@ -13,27 +16,87 @@ class ClinicDetail extends Component {
   }
 
   async componentDidMount() {
-    let specialistId = this.props.match.params.id
-    this.setState({
-      specialistId: specialistId,
-    })
-
-    let doctorResponse = await handleGetDoctorInSpecialist(specialistId)
-    if (doctorResponse && doctorResponse.errCode === 0) {
-      let dataDoctor = doctorResponse.data
-
-      this.setState({
-        filteredArrDoctor: dataDoctor,
-      })
-    }
+    // let specialistId = this.props.match.params.id
+    // this.setState({
+    //   specialistId: specialistId,
+    // })
+    // let doctorResponse = await handleGetDoctorInSpecialist(specialistId)
+    // if (doctorResponse && doctorResponse.errCode === 0) {
+    //   let dataDoctor = doctorResponse.data
+    //   this.setState({
+    //     filteredArrDoctor: dataDoctor,
+    //   })
+    // }
   }
-
   componentDidUpdate(prevProps, prevState, snapshot) {}
 
   render() {
     let filteredArrDoctor = this.state.filteredArrDoctor
     return (
       <>
+        <HeaderHomePage />
+        <div className="clinic-top-wrapper">
+          <div className="img-background">
+            <img src={img} alt="" />
+          </div>
+
+          <div className="clinic-wrapper wrapper">
+            <div className="clinic-content wrapper">
+              <div className="clinic-header">
+                <div className="clinic-information">
+                  <div className="clinic-information-left">
+                    <img className="img-logo" alt="" />
+                  </div>
+                  <div className="clinic-information-right">
+                    <div className="clinic-name-address">
+                      <h1 className="clinic-name">dsadadasds</h1>
+                      <h4>dasdasd</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="clinic-menu wrapper">
+              <div className="clinic-menu-introduction">Gioi thieu</div>
+              <div className="clinic-menu-docto">Bac Si</div>
+            </div>
+
+            <div className="clinic-information1">
+              HealthCare Consultancy is Houston's leading comprehensive
+              healthcare platform connecting users with over 200 prestigious
+              hospitals - clinics, more than 1,500 good specialists and
+              thousands of quality medical products and good services.
+            </div>
+
+            <div className="clinic-information2">
+              From now on, patients can book a clinic at the University Hospital
+              of Medicine and Pharmacy 1 through the BookingCare booking system.
+              <ul className="info-list">
+                <li>Be selected to examine with experienced specialists</li>
+                <li>
+                  Support to book online before going to the doctor (free of
+                  charge)
+                </li>
+                <li>Reduce waiting time in line for medical check-in</li>
+                <li>Receive detailed examination instructions after booking</li>
+              </ul>
+            </div>
+            <div className="clinic-doctor-list">
+              <div className="clinic-doctor-list-title clinic-title">
+                Bac Si
+              </div>
+            </div>
+
+            <div className="clinic-introduction">
+              <div className=" clinic-introduction-title clinic-title">
+                gioi thieu
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <HomeFooter />
+
         {filteredArrDoctor &&
           filteredArrDoctor.length > 0 &&
           filteredArrDoctor.map((doctor, index) => {
