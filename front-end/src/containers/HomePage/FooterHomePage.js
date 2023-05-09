ac8185836fd838cb0b55607cf4a140ff2100e248
Thanh Vu <65506586+vvt4994@users.noreply.github.com>
2023-05-09 04:28:27 -0500

    Responsive for Footer

--- front-end/src/containers/HomePage/FooterHomePage.js
+++ front-end/src/containers/HomePage/FooterHomePage.js
@@ -5,7 +5,7 @@ class FooterHomePage extends Component {
   render() {
     return (
       <div className="home-footer">
-        <div className="wrapper">
+        <div className="home-footer-wrapper">
           <p>&copy; 2023 Thanh Vu.</p>
           <a target="/blank" href="https://github.com/vvt4994">
             <i className="fab fa-github"></i>
